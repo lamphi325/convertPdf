068d94c109fd6fe962c03fdad1b282928cbd7748
lamphi <lamphi325@gmail.com>
2022-04-23 17:32:08 +0700

    add image

--- ConvertPdf/ConvertPdf/Logic/ConvertPdf.cs
+++ ConvertPdf/ConvertPdf/Logic/ConvertPdf.cs
@@ -111,38 +111,31 @@ namespace ConvertPdf.Logic
 
         public void Convert4()
         {
-            string html = @"
-                <!DOCTYPE html>
-                <html xmlns=""http://www.w3.org/1999/xhtml"" lang="""" xml:lang="""">
-                <head>
-                    <title></title>
-                    <meta http-equiv=""Content-Type"" content=""text/html""; charset=""UTF-8"" />
-                    <meta http-equiv=""X-UA-Compatible"" content=""IE=edge"" />
-                    <meta name=""viewport"" content=""width=device-width, initial-scale=1"" />
-                </head>
-                <body style=""margin: 0; padding: 0; width: 892px; height: 1448px; background-image: url(D:\\inetpub\\ConvertPdf\\ConvertPdf\\wwwroot\\image\\target001.png); background-repeat: no-repeat; background-size: cover; object-fit: fill;"">
-                       <div id =""page1-div"" style=""margin: 0; padding: 0; position: relative; width: 892px; height: 1448px;"">
-                       <p style=""position: absolute; top: 1422px; left: 707px; white-space: nowrap; font-size: 13px; font-family: Calibri; color: #07476f; margin: 0; padding: 0;"">Nhôm hệ Topal &#160; | &#160;&#160;<b>1&#160;</b></p>
-                       <p style=""position: absolute; top: 1337px; left: 142px; white-space: nowrap; font-size: 65px; font-family: Calibri; color: #ffffff; margin: 0; padding: 0;""><b>AN&#160;PHÁT</b></p>
-                       <p style=""position: absolute; top: 1307px; left: 142px; white-space: nowrap; font-size: 25px; font-family: Calibri; color: #ffffff; margin: 0; padding: 0;"">ĐẠI LÝ ỦY QUYỀN NHÔM TOPAL PRO</p>
-                       <p style=""position: absolute; top: 1305px; left: 640px; white-space: nowrap; font-size: 20px; font-family: Calibri; color: #ffffff; margin: 0; padding: 0;"">NO10-LK10-08-Khu đất DV Dọc Bún 1</p>
-                       <p style=""position: absolute; top: 1327px; left: 640px; white-space: nowrap; font-size: 20px; font-family: Calibri; color: #ffffff; margin: 0; padding: 0;"">P.La Khê, Q. Hà Đông, Hà Nội</p>
-                       <p style=""position: absolute; top: 1380px; left: 640px; white-space: nowrap; font-size: 20px; font-family: Calibri; color: #ffffff; margin: 0; padding: 0;"">0975265678</p>
-                       <p style=""position: absolute; top: 918px; left: 242px; white-space: nowrap; font-size: 65px; font-family: Calibri; color: #07476f; margin: 0; padding: 0;"">CATALOGUE&#160;</p>
-                       <p style=""position: absolute; top: 918px; left: 584px; white-space: nowrap; font-size: 65px; font-family: Calibri; color: #f58220; margin: 0; padding: 0;"">NHÔM<b>&#160;</b></p>
-                    </div>
-                </body>
-                </html>
-            ";
-            //var config = new PdfGenerateConfig();
-            //config.MarginBottom = 0;
-            //config.MarginLeft = 0;
-            //config.MarginTop = 0;
-            //config.MarginRight = 0;
-            //config.PageSize = PdfSharp.PageSize.B4;
-            //PdfSharp.Pdf.PdfDocument pdfDocument = PdfGenerator.GeneratePdf(html, config);
+            //string html = @"
+            //    <!DOCTYPE html>
+            //    <html xmlns=""http://www.w3.org/1999/xhtml"" lang="""" xml:lang="""">
+            //    <head>
+            //        <title></title>
+            //        <meta http-equiv=""Content-Type"" content=""text/html""; charset=""UTF-8"" />
+            //        <meta http-equiv=""X-UA-Compatible"" content=""IE=edge"" />
+            //        <meta name=""viewport"" content=""width=device-width, initial-scale=1"" />
+            //    </head>
+            //    <body style=""margin: 0; padding: 0; width: 892px; height: 1448px; background-image: url(D:\\inetpub\\ConvertPdf\\ConvertPdf\\wwwroot\\image\\PDFtoJPG.me-01.jpg); background-repeat: no-repeat; background-size: cover; object-fit: fill;"">
+            //           <div id =""page1-div"" style=""margin: 0; padding: 0; position: relative; width: 892px; height: 1448px;"">
+            //           <p style=""position: absolute; top: 1422px; left: 707px; white-space: nowrap; font-size: 13px; font-family: Calibri; color: #07476f; margin: 0; padding: 0;"">Nhôm hệ Topal &#160; | &#160;&#160;<b>1&#160;</b></p>
+            //           <p style=""position: absolute; top: 1337px; left: 142px; white-space: nowrap; font-size: 65px; font-family: Calibri; color: #ffffff; margin: 0; padding: 0;""><b>AN&#160;PHÁT</b></p>
+            //           <p style=""position: absolute; top: 1307px; left: 142px; white-space: nowrap; font-size: 25px; font-family: Calibri; color: #ffffff; margin: 0; padding: 0;"">ĐẠI LÝ ỦY QUYỀN NHÔM TOPAL PRO</p>
+            //           <p style=""position: absolute; top: 1305px; left: 640px; white-space: nowrap; font-size: 20px; font-family: Calibri; color: #ffffff; margin: 0; padding: 0;"">NO10-LK10-08-Khu đất DV Dọc Bún 1</p>
+            //           <p style=""position: absolute; top: 1327px; left: 640px; white-space: nowrap; font-size: 20px; font-family: Calibri; color: #ffffff; margin: 0; padding: 0;"">P.La Khê, Q. Hà Đông, Hà Nội</p>
+            //           <p style=""position: absolute; top: 1380px; left: 640px; white-space: nowrap; font-size: 20px; font-family: Calibri; color: #ffffff; margin: 0; padding: 0;"">0975265678</p>
+            //           <p style=""position: absolute; top: 918px; left: 242px; white-space: nowrap; font-size: 65px; font-family: Calibri; color: #07476f; margin: 0; padding: 0;"">CATALOGUE&#160;</p>
+            //           <p style=""position: absolute; top: 918px; left: 584px; white-space: nowrap; font-size: 65px; font-family: Calibri; color: #f58220; margin: 0; padding: 0;"">NHÔM<b>&#160;</b></p>
+            //        </div>
+            //    </body>
+            //    </html>
+            //";
+            string html = HtmlTemplate.Html;
             var file = _webHostEnvironment.WebRootPath + "\\convert1.pdf";
-            //pdfDocument.Save(file);
             SelectPdf.HtmlToPdf htmlToPdf = new SelectPdf.HtmlToPdf();
             SelectPdf.PdfDocument pdfDocument = htmlToPdf.ConvertHtmlString(html);
             pdfDocument.Save(file);
---
+++ ConvertPdf/ConvertPdf/Models/HtmlTemplate.cs
@@ -0,0 +1,30 @@
+﻿namespace ConvertPdf.Models
+{
+    public static class HtmlTemplate
+    {
+        public const string Html = @"
+            <!DOCTYPE html>
+            <html xmlns=""http://www.w3.org/1999/xhtml"" lang="""" xml:lang="""">
+            <head>
+                <title></title>
+                <meta http-equiv=""Content-Type"" content=""text/html""; charset=""UTF-8"" />
+                <meta http-equiv=""X-UA-Compatible"" content=""IE=edge"" />
+                <meta name=""viewport"" content=""width=device-width, initial-scale=1"" />
+            </head>
+            <body style=""margin: 0; padding: 0; width: 892px; height: 1448px; background-image: url(D:\\inetpub\\ConvertPdf\\ConvertPdf\\wwwroot\\image\\PDFtoJPG.me-01.jpg); background-repeat: no-repeat; background-size: cover; object-fit: fill;"">
+                    <div id =""page1-div"" style=""margin: 0; padding: 0; position: relative; width: 892px; height: 1448px;"">
+                    <p style=""position: absolute; top: 1422px; left: 707px; white-space: nowrap; font-size: 13px; font-family: Calibri; color: #07476f; margin: 0; padding: 0;"">Nhôm hệ Topal &#160; | &#160;&#160;<b>1&#160;</b></p>
+                    <p style=""position: absolute; top: 1337px; left: 142px; white-space: nowrap; font-size: 65px; font-family: Calibri; color: #ffffff; margin: 0; padding: 0;""><b>AN&#160;PHÁT</b></p>
+                    <p style=""position: absolute; top: 1307px; left: 142px; white-space: nowrap; font-size: 25px; font-family: Calibri; color: #ffffff; margin: 0; padding: 0;"">ĐẠI LÝ ỦY QUYỀN NHÔM TOPAL PRO</p>
+                    <p style=""position: absolute; top: 1305px; left: 640px; white-space: nowrap; font-size: 20px; font-family: Calibri; color: #ffffff; margin: 0; padding: 0;"">NO10-LK10-08-Khu đất DV Dọc Bún 1</p>
+                    <p style=""position: absolute; top: 1327px; left: 640px; white-space: nowrap; font-size: 20px; font-family: Calibri; color: #ffffff; margin: 0; padding: 0;"">P.La Khê, Q. Hà Đông, Hà Nội</p>
+                    <p style=""position: absolute; top: 1380px; left: 640px; white-space: nowrap; font-size: 20px; font-family: Calibri; color: #ffffff; margin: 0; padding: 0;"">0975265678</p>
+                    <p style=""position: absolute; top: 918px; left: 242px; white-space: nowrap; font-size: 65px; font-family: Calibri; color: #07476f; margin: 0; padding: 0;"">CATALOGUE&#160;</p>
+                    <p style=""position: absolute; top: 918px; left: 584px; white-space: nowrap; font-size: 65px; font-family: Calibri; color: #f58220; margin: 0; padding: 0;"">NHÔM<b>&#160;</b></p>
+                </div>
+            </body>
+            </html>
+            
+        ";
+    }
+}
